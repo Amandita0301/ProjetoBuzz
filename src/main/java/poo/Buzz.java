8621e1ef855c29f644513400578fd29980c1a5e4
Amanda <amanda_ja@hotmail.com>
2019-03-16 17:10:08 -0300

    class buzz pronta

--- src/main/java/poo/Buzz.java
+++ src/main/java/poo/Buzz.java
@@ -1,7 +1,10 @@
 package poo;
 
-public class Buzz {
+import java.lang.InterruptedException;
+import java.lang.Thread;
 
+public class Buzz {
+    int cont = 1;
     //atributos da classe
 
     private int ligar;
@@ -15,6 +18,9 @@ public class Buzz {
     //metodos
 
     public int obterEstadoCapacete() {return capacete; }
+    public int obterEstadoBraco() {return botaoBraco; }
+    public int obterEstadoFrase() {return botaoFrases; }
+
 
     public void AlteraCapacete(int capacete){
         this.capacete = capacete;
@@ -34,13 +40,61 @@ public class Buzz {
         System.out.println("Laser acionado: ");
 
         for(int i = 0; i < 10 ;i++) {
-            System.out.println("\u21A0");            //imprime caracter "laser"
+            System.out.println("\u21A0");                             //imprime "laser"
         }
-        System.out.println("\u21A0");
     }
 
     public void botaoAsas(){
+        this.botaoAsas = botaoAsas;
 
+        System.out.println("Asas abertas: ");
+        System.out.println("\u21F7" + "\u1F60"  + "\u21f8");
+    }
+
+    public void botaoBraco(int botaoBraco){
+        this.botaoBraco = botaoBraco;
+
+        if (botaoBraco == 1){                                          //se o braço estiver esticado
+            System.out.println("Dobrar o braço: ");
+            System.out.println("\u268A" + "\u25E0" + "\u268A");       //dobra ele
+        }else{
+            System.out.println("Esticar o braço: ");
+            System.out.println("\u268A" + "\u25DC" + "\u268A");       //estica ele
+        }
+
+    }
+
+    public void botaoFrases(int botaoFrases){
+        this.botaoFrases = botaoFrases;
+
+        if(cont == 7){
+            cont = 1;
+        }
+
+        try{
+        if(cont == 1){
+            System.out.println("\u1d16" + "Buzz Lighter ao seu dispor" +"\u1d16" );
+            Thread.sleep(2000);
+        }else if(cont == 2){
+            System.out.println("\u1d16" + "Diario da missao: me infiltrei no território inimigo sem ser detectado" +"\u1d16" );
+            Thread.sleep(2000);
+        }else if(cont == 3){
+            System.out.println("\u1d16" + "Pare!!! Ordeno que pare!" +"\u1d16" );
+            Thread.sleep(2000);
+        }else if(cont == 4) {
+            System.out.println("\u1d16" + "Nunca duvidei de mim mesmo comandante e nao vou começar agora" + "\u1d16");
+            Thread.sleep(2000);
+        }else if(cont == 5){
+            System.out.println("\u1d16" + "Alvo se aproximando" +"\u1d16" );
+            Thread.sleep(2000);
+        }else{
+            System.out.println("\u1d16" + "Comando estelar estou me aproximando da escotilha de fuga,preparar lançamento!" + "\u1d16");
+            Thread.sleep(2000);
+        }
+
+        }catch (InterruptedException ex) {}
+
+        cont++;
 
     }
 
--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -1,4 +1,44 @@
 package poo;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        Buzz boneco = new Buzz();
+        int opcao = 0;
+
+        System.out.println("Vamos brincar?:");
+
+        do{
+            System.out.println("Digite:");
+            System.out.println("1:Acender o Laser:");
+            System.out.println("2:Mexer as Asas:");
+            System.out.println("3:Falar Frases:");
+            System.out.println("4:Mexer os Braços:");
+            System.out.println("5:Mexer no Capacete:");
+            System.out.println("6:Encerrar a Brincadeira:");
+            opcao = teclado.nextInt();
+
+//            switch (1){
+//
+//
+//
+//            }
+//
+
+
+
+
+
+
+
+        }while(opcao != 6);
+
+
+
+
+    }
 }
