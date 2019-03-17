b7dbd797bc2be8ac73a874bd3d115c4a0266120c
Amanda <amanda_ja@hotmail.com>
2019-03-17 14:10:31 -0300

    usando os atributos

--- src/main/java/poo/Buzz.java
+++ src/main/java/poo/Buzz.java
@@ -5,7 +5,6 @@ import java.lang.Thread;
 
 public class Buzz {
 
-
     //atributos da classe
     private int capacete;
     private int botaoFrases;
@@ -14,85 +13,104 @@ public class Buzz {
     private int botaoBraco;
     int cont = 1;
 
+    public int getBotaoAsas() { return botaoAsas; }
+    public void setBotaoAsas(int botaoAsas) { this.botaoAsas = botaoAsas; }
+
+    public int getBotaoBraco() { return botaoBraco; }
+    public void setBotaoBraco(int botaoBraco) { this.botaoBraco = botaoBraco; }
+
+    public int getCapacete() { return capacete; }
+    public void setCapacete(int capacete) { this.capacete = capacete; }
+
+    public int getbotaoFrases() { return botaoFrases; }
+    public void setbotaoFrases(int botaoFrases) { this.botaoFrases = botaoFrases; }
+
+    public int getBotaoLaser() { return botaoLaser; }
+    public void setBotaoLaser(int botaoLaser) { this.botaoLaser = botaoLaser; }
+
+
     //metodos
 
+    public void alteraCapacete() {
 
-
-//    public int obterEstadoCapacete() {return capacete; }
-//    public int obterEstadoBraco() {return botaoBraco; }
-//    public int obterEstadoFrase() {return botaoFrases; }
-
-
-    public void AlteraCapacete(int capacete){
-        this.capacete = capacete;
-
-        if (capacete == 1){                                         //se o capacete estiver fechado
+        if(capacete == 1) {
             System.out.println("Capacete fechado: ");
             System.out.println();
             System.out.println("\u268A" + "\u25E0" + "\u268A");     //close
             System.out.println();
-        }else{
+        }else if(capacete == 2) {
             System.out.println();
             System.out.println("Capacete aberto: ");
             System.out.println("\u268A" + "\u25DC" + "\u268A");     //open
             System.out.println();
+        }else{
+            System.out.println("Opção Inválida:");
         }
     }
 
-    public void botaoLaser(){
-        //this.botaoLaser = botaoLaser;
+    public void alteraLaser(){
 
-        System.out.println("Laser acionado: ");
-        System.out.println();
-        System.out.println();
-        System.out.println("      " + "\u0040");                                      //LINHA 1
-        System.out.print("  " + "\u22F1" + "\u22F0"  + "\u0040" + "\u0040" + "\u22F1" + "\u22F0" );     //LINHA 2
-        for(int i = 0; i < 45 ;i++) {
-            System.out.print("\u21A0");                             //imprime "laser"
-        }System.out.println();
-        System.out.println("     " + "\u0040" + "\u0040");
-        System.out.println("     " + "\u0040" + "\u0040");
-        System.out.println();
+        if(botaoLaser == 1) {
+            System.out.println("Laser acionado: ");
+            System.out.println();
+            System.out.println("      " + "\u0040");                                      //LINHA 1
+            System.out.print("  " + "\u22F1" + "\u22F0" + "\u0040" + "\u0040" + "\u22F1" + "\u22F0");     //LINHA 2
+            for (int i = 0; i < 45; i++) {
+                System.out.print("\u21A0");                             //imprime "laser"
+            }
+            System.out.println();
+            System.out.println("     " + "\u0040" + "\u0040");
+            System.out.println("     " + "\u0040" + "\u0040");
+            System.out.println();
+        }else if (botaoLaser == 2){
+            System.out.println("Laser apagado: ");
+            System.out.println();
+            System.out.println("      " + "\u0040");                                      //LINHA 1
+            System.out.print("  " + "\u22F1" + "\u22F0" + "\u0040" + "\u0040" + "\u22F1" + "\u22F0");     //LINHA 2
+            System.out.println();
+            System.out.println("     " + "\u0040" + "\u0040");
+            System.out.println("     " + "\u0040" + "\u0040");
+        }else{
+            System.out.println("Opção Inválida:");
+        }
     }
 
-    public void botaoAsas(int botaoAsas){
-        this.botaoAsas = botaoAsas;
+    public void alteraAsas() {
 
-        if(botaoAsas == 1){
+        if(botaoAsas ==1) {
             System.out.println("Asas abertas: ");
             System.out.println();
             System.out.println("     " + "\u0040");
-            System.out.println(" " + "\u21F7" + "\u21F7" + "\u22F0"  + "\u0040" + "\u0040" + "\u22F1" + "\u21F7" + "\u21f8" );     //LINHA 2
-            System.out.println(" " + "\u22F0" + "  "+ "\u0040" + "\u0040"+ "  " + "\u22F1" );
+            System.out.println(" " + "\u21F7" + "\u21F7" + "\u22F0" + "\u0040" + "\u0040" + "\u22F1" + "\u21F7" + "\u21f8");     //LINHA 2
+            System.out.println(" " + "\u22F0" + "  " + "\u0040" + "\u0040" + "  " + "\u22F1");
             System.out.println("     " + "\u0040" + "\u0040");
             System.out.println("     " + "\u0040" + "\u0040");
             System.out.println();
-
-        }else{
+        }else if (botaoAsas == 2) {
             System.out.println("Asas fechadas: ");
             System.out.println();
             System.out.println("     " + "\u0040");                                          //LINHA 1
-            System.out.println("   " + "\u22F0"  + "\u0040" + "\u0040" + "\u22F1" );             //LINHA 2
-            System.out.println(" " + "\u22F0" + " " + "\u21DF" + "\u0040" + "\u0040"+ "\u21DF" + " " + "\u22F1" );                    //linha3
-            System.out.println("    " + "\u21DF" + "\u0040" + "\u0040"+ "\u21DF");
-            System.out.println("    " + "\u21DF" + "\u0040" + "\u0040"+ "\u21DF");
+            System.out.println("   " + "\u22F0" + "\u0040" + "\u0040" + "\u22F1");             //LINHA 2
+            System.out.println(" " + "\u22F0" + " " + "\u21DF" + "\u0040" + "\u0040" + "\u21DF" + " " + "\u22F1");                    //linha3
+            System.out.println("    " + "\u21DF" + "\u0040" + "\u0040" + "\u21DF");
+            System.out.println("    " + "\u21DF" + "\u0040" + "\u0040" + "\u21DF");
             System.out.println();
+        }else{
+            System.out.println("Opção Inválida:");
         }
-
     }
 
-    public void botaoBraco(int botaoBraco){
-        this.botaoBraco = botaoBraco;
 
-        if (botaoBraco == 1){                                          //se o braço estiver esticado
+    public void alteraBraco(){
+        if(botaoBraco == 1) {
             System.out.println("Dobrar o braço: ");
             System.out.println();
             System.out.println("      " + "\u0040");                                      //LINHA 1
-            System.out.println("  " + "\u22F1" + "\u22F0"  + "\u0040" + "\u0040" + "\u22F1" + "\u22F0" );     //LINHA 2
+            System.out.println("  " + "\u22F1" + "\u22F0" + "\u0040" + "\u0040" + "\u22F1" + "\u22F0");     //LINHA 2
             System.out.println("     " + "\u0040" + "\u0040");
             System.out.println("     " + "\u0040" + "\u0040");
-            System.out.println();
-        }else{
+            System.out.println();                                                 //se o braço estiver esticado
+        }else if(botaoBraco == 2){
             System.out.println("Esticar o braço: ");
             System.out.println();
             System.out.println("     " + "\u0040");                                      //LINHA 1
@@ -101,54 +119,45 @@ public class Buzz {
             System.out.println("     " + "\u0040" + "\u0040");
             System.out.println("     " + "\u0040" + "\u0040");
             System.out.println();
+        }else{
+            System.out.println("Opção Inválida:");
         }
-
     }
 
-    public void botaoFrases(){
-        //this.botaoFrases = botaoFrases;
+    public void exibeFrases(){
 
+        if(botaoFrases == 1){
         if(cont == 7){
             cont = 1;
         }
 
-        try{
+
         if(cont == 1){
             System.out.println();
             System.out.println("Buzz Lighter ao seu dispor");
             System.out.println();
-            Thread.sleep(2000);
         }else if(cont == 2){
             System.out.println();
             System.out.println("Diario da missao: me infiltrei no território inimigo sem ser detectado");
             System.out.println();
-            Thread.sleep(2000);
         }else if(cont == 3){
             System.out.println();
             System.out.println("Pare!!! Ordeno que pare!");
             System.out.println();
-            Thread.sleep(2000);
         }else if(cont == 4) {
             System.out.println();
             System.out.println("Nunca duvidei de mim mesmo comandante e nao vou começar agora");
             System.out.println();
-            Thread.sleep(2000);
         }else if(cont == 5){
             System.out.println();
             System.out.println("Alvo se aproximando");
             System.out.println();
-            Thread.sleep(2000);
         }else{
             System.out.println();
             System.out.println("Comando estelar estou me aproximando da escotilha de fuga,preparar lançamento!");
             System.out.println();
-            Thread.sleep(2000);
-        }
-
-        }catch (InterruptedException ex) {}
-
-        cont++;
-
+        }cont++;
+      }
     }
 
 }
--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -14,43 +14,53 @@ public class Principal {
 
         do{
             System.out.println("Digite:");
-            System.out.println("1:Acender o Laser:");
+            System.out.println("1:Mexer no Laser:");
             System.out.println("2:Mexer as Asas:");
             System.out.println("3:Mexer no capacete:");
             System.out.println("4:Mexer os Braços:");
-            System.out.println("5:Falar Frases:");
+            System.out.println("5:Ler as Frases:");
             System.out.println("6:Encerrar a Brincadeira:");
             opcao = teclado.nextInt();
 
             switch (opcao)
             {
                 case 1:
-                    boneco.botaoLaser();
+                    System.out.println("Digite:");
+                    System.out.println("1:Para acender o laser");
+                    System.out.println("2:Para apagar o laser");
+                    boneco.setBotaoLaser(teclado.nextInt());
+                    boneco.alteraLaser();
                     break;
 
                 case 2:
                     System.out.println("Digite:");
                     System.out.println("1:Para abrir as asas");
                     System.out.println("2:Para fechar as asas");
-                    boneco.botaoAsas(teclado.nextInt());
+                    boneco.setBotaoAsas(teclado.nextInt());
+                    boneco.alteraAsas();
                     break;
 
                 case 3:
                     System.out.println("Digite:");
                     System.out.println("1:Para fechar o capacete");
                     System.out.println("2:Para abrir o capacete");
-                    boneco.AlteraCapacete(teclado.nextInt());
+                    boneco.setCapacete(teclado.nextInt());
+                    boneco.alteraCapacete();
                     break;
 
                 case 4:
                     System.out.println("Digite:");
                     System.out.println("1:Para dobrar os braços:");
                     System.out.println("2:Para esticar os braços:");
-                    boneco.botaoBraco(teclado.nextInt());
+                    boneco.setBotaoBraco(teclado.nextInt());
+                    boneco.alteraBraco();
                     break;
 
                 case 5:
-                    boneco.botaoFrases();
+                    System.out.println("Digite:");
+                    System.out.println("1:Para ouvir uma frase:");
+                    boneco.setbotaoFrases(teclado.nextInt());
+                    boneco.exibeFrases();
                     break;
 
                 default:
@@ -60,7 +70,7 @@ public class Principal {
 
         }while(opcao != 6);
 
-        System.out.println("Acabou a brinadeira");
+        System.out.println("Acabou a brincadeira");
 
 
     }
